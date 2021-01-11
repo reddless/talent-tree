57e5c9627091765bc9fb9b704a6f8780fb70463b
reddless <root@DESKTOP-RSMB1CP.localdomain>
2021-01-11 04:15:53 -0800

    Updates

---
+++ precache-manifest.91a12c1b14d2b8b23859137d0ab3db97.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06961da001827aced89da157d5805947",
+    "url": "/talent-tree/index.html"
+  },
+  {
+    "revision": "4c1d3fce9185b577ff7d",
+    "url": "/talent-tree/static/css/0.aca427fe.chunk.css"
+  },
+  {
+    "revision": "f18b010cd6caa1982664",
+    "url": "/talent-tree/static/css/main.5ecb3537.chunk.css"
+  },
+  {
+    "revision": "4c1d3fce9185b577ff7d",
+    "url": "/talent-tree/static/js/0.eb2af761.chunk.js"
+  },
+  {
+    "revision": "5d2a038b450958fa53e0",
+    "url": "/talent-tree/static/js/10.016e0aaf.chunk.js"
+  },
+  {
+    "revision": "4ac66f0cb9e3e77c7131",
+    "url": "/talent-tree/static/js/11.adcce318.chunk.js"
+  },
+  {
+    "revision": "3611a7d5a35c185dc18a",
+    "url": "/talent-tree/static/js/12.2dcca183.chunk.js"
+  },
+  {
+    "revision": "2f33dc5ac6616a7770b8117950a39b8a",
+    "url": "/talent-tree/static/js/12.2dcca183.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91abfa21f6b6dcab4a2b",
+    "url": "/talent-tree/static/js/3.95ff0e4e.chunk.js"
+  },
+  {
+    "revision": "d8a61ab6420b6cb12246",
+    "url": "/talent-tree/static/js/4.4596a7c3.chunk.js"
+  },
+  {
+    "revision": "5726d0170d64d20d3c65",
+    "url": "/talent-tree/static/js/5.176b640d.chunk.js"
+  },
+  {
+    "revision": "2d86db439f7e6a84d0e8",
+    "url": "/talent-tree/static/js/6.d6147b48.chunk.js"
+  },
+  {
+    "revision": "446bb9b4550901015038",
+    "url": "/talent-tree/static/js/7.d68f298b.chunk.js"
+  },
+  {
+    "revision": "e7a10ebfa749bfc6abc8",
+    "url": "/talent-tree/static/js/8.77c8bbb5.chunk.js"
+  },
+  {
+    "revision": "3967376559c1989d2520",
+    "url": "/talent-tree/static/js/9.b3c40a88.chunk.js"
+  },
+  {
+    "revision": "f18b010cd6caa1982664",
+    "url": "/talent-tree/static/js/main.547d01f7.chunk.js"
+  },
+  {
+    "revision": "fe6c0b15bd45a4570d1d",
+    "url": "/talent-tree/static/js/runtime-main.7260e03e.js"
+  },
+  {
+    "revision": "68b29a926e1bebd38e63343d7305e18b",
+    "url": "/talent-tree/static/media/affliction.68b29a92.jpg"
+  },
+  {
+    "revision": "892c508ab326b2ad9c2e99fa8d449b8b",
+    "url": "/talent-tree/static/media/arcane.892c508a.jpg"
+  },
+  {
+    "revision": "3b1eb09954b17a81158141f6e8904d66",
+    "url": "/talent-tree/static/media/arms.3b1eb099.jpg"
+  },
+  {
+    "revision": "2ba9eac3be306825332ee45dc3ea95ee",
+    "url": "/talent-tree/static/media/assassination.2ba9eac3.jpg"
+  },
+  {
+    "revision": "e0beb1ab57f18f7e840de64bb3643658",
+    "url": "/talent-tree/static/media/balance.e0beb1ab.jpg"
+  },
+  {
+    "revision": "1e37fb0bdd7dcf042bbb1d90e7e5283e",
+    "url": "/talent-tree/static/media/beast-mastery.1e37fb0b.jpg"
+  },
+  {
+    "revision": "732693ea62975ccea098dcdc16e4c0b8",
+    "url": "/talent-tree/static/media/combat.732693ea.jpg"
+  },
+  {
+    "revision": "fd92e13b4f078274ec04ed46d4fae453",
+    "url": "/talent-tree/static/media/demonology.fd92e13b.jpg"
+  },
+  {
+    "revision": "b0e11336eb5bf1ae15fbc446fb3cf1b4",
+    "url": "/talent-tree/static/media/destruction.b0e11336.jpg"
+  },
+  {
+    "revision": "7ef542fd8d94cd6054e6a90f30d3716c",
+    "url": "/talent-tree/static/media/discipline.7ef542fd.jpg"
+  },
+  {
+    "revision": "ec420cae1396f99d4408709f24689863",
+    "url": "/talent-tree/static/media/elemental.ec420cae.jpg"
+  },
+  {
+    "revision": "968052c44f47ad9d392a75dc037ce025",
+    "url": "/talent-tree/static/media/enhancement.968052c4.jpg"
+  },
+  {
+    "revision": "b6a8478ee749da241e033f269585b888",
+    "url": "/talent-tree/static/media/feral.b6a8478e.jpg"
+  },
+  {
+    "revision": "fbfd140b8e2c1824b3277400a9683f70",
+    "url": "/talent-tree/static/media/fire.fbfd140b.jpg"
+  },
+  {
+    "revision": "514eba87a20832346a212ec52ae3c152",
+    "url": "/talent-tree/static/media/frost.514eba87.jpg"
+  },
+  {
+    "revision": "5a9a21e2be8f6171ae0990d757115bec",
+    "url": "/talent-tree/static/media/fury.5a9a21e2.jpg"
+  },
+  {
+    "revision": "514967219f3500ea582a205697acbd4c",
+    "url": "/talent-tree/static/media/holy.51496721.jpg"
+  },
+  {
+    "revision": "b7f1381d53512475292ba07cdf38d28d",
+    "url": "/talent-tree/static/media/holy.b7f1381d.jpg"
+  },
+  {
+    "revision": "0c9de8230cfedb8caaec5897d4a4ecb3",
+    "url": "/talent-tree/static/media/left--gold.0c9de823.png"
+  },
+  {
+    "revision": "e97d48addad74d2ba0814e7446146bbf",
+    "url": "/talent-tree/static/media/left.e97d48ad.png"
+  },
+  {
+    "revision": "905dc6290a10ff93ccf8cbe3ef7bae1f",
+    "url": "/talent-tree/static/media/marksmanship.905dc629.jpg"
+  },
+  {
+    "revision": "5a8a316871d45c2cd4e43619cae4ee47",
+    "url": "/talent-tree/static/media/protection.5a8a3168.jpg"
+  },
+  {
+    "revision": "d8ff0256f76094710865faa62f8aa08f",
+    "url": "/talent-tree/static/media/protection.d8ff0256.jpg"
+  },
+  {
+    "revision": "1a85983505591136cf6a34062d8f47f0",
+    "url": "/talent-tree/static/media/restoration.1a859835.jpg"
+  },
+  {
+    "revision": "e148424a4a100d7c14fe5b058cb0dd88",
+    "url": "/talent-tree/static/media/restoration.e148424a.jpg"
+  },
+  {
+    "revision": "17666e6ce0a9de255bd68bf20ea69469",
+    "url": "/talent-tree/static/media/retribution.17666e6c.jpg"
+  },
+  {
+    "revision": "19c7b9ba4006a4f8727dcc1595260ec0",
+    "url": "/talent-tree/static/media/shadow.19c7b9ba.jpg"
+  },
+  {
+    "revision": "3d6c94d0133968629b28b172d0dae3e0",
+    "url": "/talent-tree/static/media/subtlety.3d6c94d0.jpg"
+  },
+  {
+    "revision": "95ffdde50a6e9927d03347ef6a473dec",
+    "url": "/talent-tree/static/media/survival.95ffdde5.jpg"
+  },
+  {
+    "revision": "e0f7232022e748a2b5b0e69b23d6c2cf",
+    "url": "/talent-tree/static/media/survival_instincts.e0f72320.jpg"
+  }
+]);
